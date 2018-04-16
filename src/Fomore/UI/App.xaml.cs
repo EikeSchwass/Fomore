e90bd5aa4511716333befb9814e1e1796e29a4b9
Eike <eike.stein@stud.uni-goettingen.de>
2018-04-16 21:27:44 +0200

    SplashScreen added

--- src/Fomore/UI/App.xaml.cs
+++ src/Fomore/UI/App.xaml.cs
@@ -1,6 +1,17 @@
-﻿namespace Fomore.UI
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Fomore.UI
 {
     public partial class App
     {
+        private void ApplicationStartUp(object sender, StartupEventArgs e)
+        {
+            var splashScreen = new SplashScreen("assets/images/splash.png");
+            splashScreen.Show(false);
+            Task.Delay(2500).Wait();
+            splashScreen.Close(TimeSpan.Zero);
+        }
     }
 }
