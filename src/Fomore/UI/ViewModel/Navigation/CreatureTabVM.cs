57f0c51df32be09d2b3ec310bc5e5ffa53ef9253
gulzaib.amjad <gulzaib.amjed@gmail.com>
2018-06-10 02:40:37 +0200

    Create new Creature Popup

--- src/Fomore/UI/ViewModel/Navigation/CreatureTabVM.cs
+++ src/Fomore/UI/ViewModel/Navigation/CreatureTabVM.cs
@@ -3,6 +3,14 @@
     public class CreatureTabVM : TabPageVM
     {
         /// <inheritdoc />
-        public override string Header => "Creature";
+        public override string Header => "New Creature";
+        public string EnterName => "Enter Name*:";
+        public string Description => "Description";
+        public string CreateButton => "Create";
+        public string CancelButton => "Cancel";
+
+        public string CreatureName { get; set; }
+        public string EnterDescription { get; set; }    
+
     }
 }
--- src/Fomore/UI/Views/Controls/CreatureTabControl.xaml.cs
+++ src/Fomore/UI/Views/Controls/CreatureTabControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Fomore.UI.Views.Controls
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Fomore.UI.Views.Controls
 {
     /// <summary>
     /// Interaction logic for CreatureTabControl.xaml
@@ -9,5 +13,34 @@
         {
             InitializeComponent();
         }
+
+        private void CreateNewButton_Click(object sender, RoutedEventArgs e)
+        {
+            // CoolButton Clicked! Let's show our InputBox.
+            CreatureNewBox.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // YesButton Clicked! Let's hide our InputBox and handle the input text.
+            CreatureNewBox.Visibility = System.Windows.Visibility.Collapsed;
+
+            // Do something with the Input
+            String input = InputNameTextBox.Text;
+            MyListBox.Items.Add(input); // Add Input to our ListBox.
+
+            // Clear InputBox.
+            InputNameTextBox.Text = String.Empty;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // NoButton Clicked! Let's hide our InputBox.
+            CreatureNewBox.Visibility = System.Windows.Visibility.Collapsed;
+
+            // Clear InputBox.
+            InputNameTextBox.Text = String.Empty;
+        }
+
     }
 }
