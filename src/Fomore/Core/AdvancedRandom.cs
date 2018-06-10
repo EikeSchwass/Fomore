10dd5b30ddf58040dffb01e78b74adacc4c3adde
Eike Stein <eike.stein@stud.uni-goettingen.de>
2018-06-10 20:50:20 +0200

    Implemented the evolutionary algorithm.

--- src/Fomore/Core/AdvancedRandom.cs
+++ src/Fomore/Core/AdvancedRandom.cs
@@ -1,4 +1,26 @@
-﻿namespace Core
+﻿using System;
+
+namespace Core
 {
-    public class AdvancedRandom { }
+    public static class AdvancedRandom
+    {
+        [ThreadStatic] private static Random local;
+        private static Random Global { get; } = new Random();
+
+        public static Random Random
+        {
+            get
+            {
+                if (local == null)
+                {
+                    int seed;
+                    lock (Global)
+                        seed = Global.Next();
+                    local = new Random(seed);
+                }
+
+                return local;
+            }
+        }
+    }
 }
---
+++ src/Fomore/Core/Training/Evolution/Individual.cs
@@ -0,0 +1,18 @@
+﻿// Eike Stein: Fomore/Core/EvolutionaryAlgorithm.cs (2018/06/09)
+
+
+namespace Core.Training.Evolution
+{
+    public class Individual<T>
+    {
+        public T Phenotype { get; }
+        public double? Fitness { get; }
+
+        public Individual(T phenotype, double? fitness)
+        {
+            Fitness = fitness;
+            Phenotype = phenotype;
+        }
+
+    }
+}
