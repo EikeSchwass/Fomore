c42d2d642193495c28839fa5bd335c170fae27e7
jazib.zafar <jazib.zafar@gmail.com>
2018-06-11 23:55:58 +0200

    pulled and merged. hopefully correctly this time around.

--- src/Fomore/UI/ViewModel/Navigation/EnvironmentTabVM.cs
+++ src/Fomore/UI/ViewModel/Navigation/EnvironmentTabVM.cs
@@ -8,5 +8,24 @@ namespace Fomore.UI.ViewModel.Navigation
 
         /// <inheritdoc />
         public override string Header => "Environment";
+
+        public string EnterName => "Enter Name*:";
+        public string Description => "Description";
+
+        public string Terrain => "Terrain:";
+        public string GroundType => "Ground Type:";
+        public string Gravity => "Gravity:";
+        public string Friction => "Friction:";
+
+        public string CreateButton => "Create";
+        public string CancelButton => "Cancel";
+
+        public string EnvironmentName { get; set; }
+        public string EnterDescription { get; set; }
+
+        public EnvironmentTabVM(EntityStorageVM entitiesStorage)
+        {
+            EntitiesStorage = entitiesStorage;
+        }
     }
 }
