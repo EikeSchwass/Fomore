352d8021bcf463f8a4574bba51dd9171aebab647
Eike Stein <eike.stein@stud.uni-goettingen.de>
2018-06-13 23:03:33 +0200

    Removed unnessary dependency

--- src/Fomore/UI/Views/Windows/MainWindow.xaml.cs
+++ src/Fomore/UI/Views/Windows/MainWindow.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace Fomore.UI.Views.Windows
+﻿using System.Windows;
+using Fomore.UI.ViewModel.Application;
+
+namespace Fomore.UI.Views.Windows
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)sender;
+            var mainWindowDataContext = (AppStateVM)mainWindow.DataContext;
+            mainWindowDataContext.LoadCommand?.Execute(null);
         }
     }
 }
