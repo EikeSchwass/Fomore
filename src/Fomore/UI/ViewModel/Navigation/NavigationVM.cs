30d776bcca739f36c8d9e440a382006c6aa483be
Dominick Leppich <dominick.leppich@gmail.com>
2018-06-15 18:27:27 +0200

    add missing TabNavigationVM class

--- src/Fomore/UI/ViewModel/Navigation/NavigationVM.cs
+++ src/Fomore/UI/ViewModel/Navigation/NavigationVM.cs
@@ -70,4 +70,29 @@ namespace Fomore.UI.ViewModel.Navigation
             SelectedTab = CreatureTab;
         }
     }
+
+    public class TabNavigationVM : ViewModelBase
+    {
+        public DelegateCommand SwitchToCreatureTabCommand { get; }
+        public DelegateCommand SwitchToEnvironmentTabCommand { get; }
+        public DelegateCommand SwitchToTrainingTabCommand { get; }
+        public DelegateCommand SwitchToSimulationTabCommand { get; }
+
+        /// <inheritdoc />
+        public TabNavigationVM(DelegateCommand switchToCreatureTabCommand, DelegateCommand switchToEnvironmentTabCommand, DelegateCommand switchToTrainingTabCommand, DelegateCommand switchToSimulationTabCommand)
+        {
+            SwitchToCreatureTabCommand = switchToCreatureTabCommand;
+            SwitchToEnvironmentTabCommand = switchToEnvironmentTabCommand;
+            SwitchToTrainingTabCommand = switchToTrainingTabCommand;
+            SwitchToSimulationTabCommand = switchToSimulationTabCommand;
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            SwitchToCreatureTabCommand.OnCanExecuteChanged();
+            SwitchToEnvironmentTabCommand.OnCanExecuteChanged();
+            SwitchToTrainingTabCommand.OnCanExecuteChanged();
+            SwitchToSimulationTabCommand.OnCanExecuteChanged();
+        }
+    }
 }
