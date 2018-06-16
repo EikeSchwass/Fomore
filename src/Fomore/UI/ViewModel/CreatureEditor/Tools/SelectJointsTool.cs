2479c2aafb087bd9a2d0c2ce8eaef8163fb7dac0
Eike Stein <eike.stein@stud.uni-goettingen.de>
2018-06-16 21:19:08 +0200

    Started work on selection tools

---
+++ src/Fomore/UI/ViewModel/CreatureEditor/Tools/SelectJointsTool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Fomore.UI.ViewModel.CreatureEditor.Tools
+{
+    public class SelectJointsTool : SelectAllTool
+    {
+        /// <inheritdoc />
+        public override ImageSource Image { get; } = new BitmapImage(new Uri("/assets/images/arrowselectionjoints.png", UriKind.Relative));
+
+        /// <inheritdoc />
+        public SelectJointsTool(CreatureStructureEditorCanvasVM canvasVM) : base(canvasVM) { }
+    }
+}
