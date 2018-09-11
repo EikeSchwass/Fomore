c4909a3341cff86a7393f4919bf557c763964564
Dominick Leppich <dominick.leppich@gmail.com>
2018-09-11 15:03:45 +0200

    First draft of Movement Pattern list inside of Creature overview

---
+++ src/Fomore/UI/Converter/NonEmptyCollectionConverter.cs
@@ -0,0 +1,26 @@
+﻿// Eike Stein: Fomore/UI/NumberVisibilityConverter.cs (2018/05/12)
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Fomore.UI.ViewModel.Data;
+
+namespace Fomore.UI.Converter
+{
+    /// <summary>
+    /// Like this
+    /// </summary>
+    public class NonEmptyCollectionConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value != null && Equals(((ICollection<MovementPatternVM>) value).Count, 0) ? Visibility.Collapsed : Visibility.Visible;
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
