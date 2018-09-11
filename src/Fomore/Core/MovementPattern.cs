5cbcda3e0509059ef2ba60437c6dbdd73f14ffc3
Dominick Leppich <dominick.leppich@gmail.com>
2018-09-11 13:00:59 +0200

    Move Properties of MovementPattern to Core Class

--- src/Fomore/Core/MovementPattern.cs
+++ src/Fomore/Core/MovementPattern.cs
@@ -5,6 +5,36 @@ namespace Core
     [Serializable]
     public class MovementPattern
     {
+        private string name;
+        private int iterations;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnAccess();
+            }
+        }
+
+        public int Iterations
+        {
+            get => iterations;
+            set
+            {
+                iterations = value;
+                OnAccess();
+            }
+        }
+
+        public DateTime LastAccess { get; private set; }
+
+        private void OnAccess()
+        {
+            LastAccess = DateTime.Now;
+        }
+
         public MovementPattern Clone()
         {
             return new MovementPattern();
--- src/Fomore/UI/ViewModel/Data/MovementPatternVM.cs
+++ src/Fomore/UI/ViewModel/Data/MovementPatternVM.cs
@@ -5,49 +5,29 @@ namespace Fomore.UI.ViewModel.Data
 {
     public class MovementPatternVM : ViewModelBase<MovementPattern>
     {
-        private string name;
-        private DateTime lastAccess;
-        private int iterations;
-
         public string Name
         {
-            get => name;
+            get => Model.Name;
             set
             {
-                if (value == name) return;
-                name = value;
+                Model.Name = value;
                 OnPropertyChanged();
-                OnAccess();
+                OnPropertyChanged(nameof(LastAccess));
             }
         }
 
         public int Iterations
         {
-            get => iterations;
+            get => Model.Iterations;
             set
             {
-                if (value == iterations) return;
-                iterations = value;
+                Model.Iterations = value;
                 OnPropertyChanged();
-                OnAccess();
+                OnPropertyChanged(nameof(LastAccess));
             }
         }
 
-        public DateTime LastAccess
-        {
-            get => lastAccess;
-            private set
-            {
-                if (value.Equals(lastAccess)) return;
-                lastAccess = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private void OnAccess()
-        {
-            LastAccess = DateTime.Now;
-        }
+        public DateTime LastAccess => Model.LastAccess;
 
         /// <inheritdoc />
         public MovementPatternVM(MovementPattern model) : base(model) { }
